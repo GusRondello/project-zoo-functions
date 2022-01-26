b76b672751c2ccc0c212ff5cb58ebcff6d52f346
Gustavo Rondello <gustavorondello99@outlook.com>
2022-01-26 18:47:51 -0300

    Requisito 3 completo

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,14 +1,20 @@
 const data = require('../data/zoo_data');
 const { employees } = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  const primeiroNome = employees[.firstName];
-  if (employeeName === undefined) {
-    return {};
-  } if (employeeName === primeiroNome) {
-    return { id, firstName, lastName, managers, responsibleFor };
-  }
+function getEmployeeByName(name) {
+  return name === undefined ? {} : employees.find(({ firstName, lastName }) => firstName === name || lastName === name);
 }
+/* if (employeeName === undefined) {
+  return {};
+}
+const employeer = employees.find(({ firstName, lastName }) => {
+  if (firstName === employeeName) {
+    return employeer;
+  } if (lastName === employeeName) {
+    return employeer;
+  } return employeer;
+}); */
+
 console.log(getEmployeeByName('Emery'));
 
 module.exports = getEmployeeByName;
