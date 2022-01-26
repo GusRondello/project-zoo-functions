45ebb000b8b5c3bc9897d862e0051c0bb9795850
Gustavo Rondello <gustavorondello99@outlook.com>
2022-01-26 18:51:22 -0300

    Resolvido problemas do lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,19 +2,8 @@ const data = require('../data/zoo_data');
 const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(name) {
-  return name === undefined ? {} : employees.find(({ firstName, lastName }) => firstName === name || lastName === name);
+  return name === undefined ? {} : employees
+    .find(({ firstName, lastName }) => firstName === name || lastName === name);
 }
-/* if (employeeName === undefined) {
-  return {};
-}
-const employeer = employees.find(({ firstName, lastName }) => {
-  if (firstName === employeeName) {
-    return employeer;
-  } if (lastName === employeeName) {
-    return employeer;
-  } return employeer;
-}); */
-
-console.log(getEmployeeByName('Emery'));
 
 module.exports = getEmployeeByName;
