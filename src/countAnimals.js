fc1c42f0f4273382e0abbb90b56608f18e68db3e
Gustavo Rondello <gustavorondello99@outlook.com>
2022-01-27 16:24:46 -0300

    Requisito 5 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(...animal) {
+  if (animal.length === 0) {
+    const todosAnimais = {};
+    species.forEach((especie) => { todosAnimais[especie.name] = especie.residents.length; });
+    return todosAnimais;
+  }
+  const [{ specie: value, sex: sexo }] = animal;
+  const filtraEspecie = species.find((specie) => specie.name === value).residents;
+
+  if (!sexo) {
+    return filtraEspecie.length;
+  } const filtraPorSexo = filtraEspecie.filter((residente) => residente.sex === sexo);
+  return filtraPorSexo.length;
 }
 
 module.exports = countAnimals;
