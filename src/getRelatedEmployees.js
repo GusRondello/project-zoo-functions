821a89dffaec1df86ea2aabb4ee24015ac21ef15
Gustavo Rondello <gustavorondello99@outlook.com>
2022-01-27 15:16:16 -0300

    Requisito 4 completo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,18 +1,19 @@
-// const { employees } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-// -----achar os managers------
-// 1 - verificar se o employees.id pertence a um manager
-// 1.1 -
-// 2 - se sim atribuir a managerArray
-// ----comparar se o id passado contem em managerArray----
-
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const managerEmployees = [];
+    const filtro = employees.filter((employee) => employee.managers.includes(managerId));
+    filtro.forEach((nomeCompleto) =>
+      managerEmployees.push(`${nomeCompleto.firstName} ${nomeCompleto.lastName}`));
+    return managerEmployees;
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
-
 console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   const animals = [];
-  ids.forEach((animalId) => animals.push(data.species.find((value) => value.id === animalId)));
+  ids.forEach((animalId) => animals.push(species.find((value) => value.id === animalId)));
   return animals;
 }
 // com ajuda da Ana na mentoria do dia 1 entendi como poderia voltar esse resultado no forEach.
