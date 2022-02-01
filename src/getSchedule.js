3056dbf07f37418920a295549e71a3d8da08d97b
Gustavo Rondello <gustavorondello99@outlook.com>
2022-01-31 22:54:21 -0300

    Requisito 8 completo

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,50 @@
-const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
+
+// Com ajuda de Lalá consegui utilizar os HOF's certos para as funções abaixo.
+
+const weekDays = Object.keys(hours);
+const especies = species.map((currentAnimal) => currentAnimal.name);
+
+function fillterAnimalByDay(day) {
+  return species.filter((dia) => dia.availability.includes(day)).map((nome) => nome.name);
+}
+
+const creatWeekObject = (dayOfWeek) => {
+  if (dayOfWeek === 'Monday') {
+    return { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  }
+  const daySchedule = {
+    officeHour: `Open from ${hours[dayOfWeek].open}am until ${hours[dayOfWeek].close}pm`,
+    exhibition: fillterAnimalByDay(dayOfWeek) };
+  return daySchedule;
+};
+
+function weekSchedule() {
+  return weekDays.reduce((acc, current) => {
+    acc[current] = creatWeekObject(current);
+    return acc;
+  }, {});
+}
+
+function animalSchedule(animal) {
+  return species.find((currentAnimal) => currentAnimal.name === animal).availability;
+}
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (scheduleTarget === 'Monday') {
+    return { Monday: creatWeekObject(scheduleTarget) };
+  }
+  if (weekDays.includes(scheduleTarget)) {
+    const returnObject = {};
+    returnObject[scheduleTarget] = creatWeekObject(scheduleTarget);
+    return returnObject;
+  }
+  if (especies.includes(scheduleTarget)) {
+    return animalSchedule(scheduleTarget);
+  }
+  return weekSchedule();
 }
+
+console.log(getSchedule());
 
 module.exports = getSchedule;
